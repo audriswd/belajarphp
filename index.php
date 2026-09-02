d4ee0587186f6d7d76d34065038459c26ecfa730
audriswd <24430272_audris_g_g@widyadharma.ac.id>
2026-09-02 19:21:51 +0700

    versi 2

--- index.php
+++ index.php
@@ -6,6 +6,31 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+    $nama = "Budi";
+    $umur = 30;
+    $sudahmenikah = false;
+
+    // buat kalimat nama saya budi , umur saya 30 tahun
+    // echo "nama $nama, umur $umur tahun";
+
+
+    // if ($sudahmenikah == true) {
+    //     echo "sudah menikah";
+    //  }
+    //     echo "belum menikah";
+
+    // 123 berurutan
+    // for ($i=0; $i < 10; $i++) {
+    //     echo "<br>";
+    //     echo $i;
+    // }
+
+    //tampilkan bil ganjil 1 3 5
+    // for ($i=1; $i < 10; $i+=2) {
+    //         echo $i;
+    //     }
+
+     ?> 
 </body>
 </html>
